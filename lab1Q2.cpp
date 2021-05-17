c425b2054b0e13ae1e71d09034f60e1ba2df3eee
Michael Valeriani <michael@Michaels-MacBook-Pro.local>
2021-05-16 17:11:31 -0700

    add struct question

---
+++ lab1Q2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+// Define a structure type FractionT to represent fractions like 1/2 and 3/5.
+
+struct FractionT {
+	int numerator;
+	int denominator;
+}
