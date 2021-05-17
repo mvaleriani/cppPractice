1241171b28cba1ae4bb107d0dc88259a90169433
Michael Valeriani <michael@Michaels-MacBook-Pro.local>
2021-05-16 17:32:21 -0700

    add iseven question

---
+++ lab1Q3.cpp
@@ -0,0 +1,28 @@
+include <iostream> 
+include <string> 
+using namespace std;
+
+/*
+Write a program that inputs a series of integers and passes them one at a time to function isEven, which uses the modulus operator to determine whether an integer is even. The function should take an integer argument and return true if the integer is even and false otherwise.
+*/
+
+
+bool IsEven ( int toCheck ) {
+	return ( toCheck % 2 == 0 );
+}
+
+int main() {
+   	string userInt;    
+	
+	cout << "Input an integer to check if it is even. Input 'q' when done" << endl;
+        getline(cin, userInt);
+
+
+	while ( userInt != "q" ) {
+        	cout << userInt << " is an" << ( IsEven( stoi( userInt ) ) ? " even " : " odd" ) << " number" << ends;
+		
+		cout << "Input an integer to check if it is even. Input 'q' when done" << endl;
+          	getline(cin, userInt);
+	} 
+
+}
